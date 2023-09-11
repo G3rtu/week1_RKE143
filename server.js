cd1858be54d071c6828f86c2d8748a5eeae346a0
Gertu Piho <gertu.piho@tktk.ee>
2023-09-11 12:23:25 +0300

    week 1 code

---
+++ server.js
@@ -0,0 +1,12 @@
+//console.log("Hello, world!");
+let firstName = 'Harry';
+//console.log(firstName);
+let lastName = "Potter";
+//console.log("Hello, " + firstName + "!");
+
+//template literals
+
+//console.log(`Hello, ${firstName}!`) ;
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
